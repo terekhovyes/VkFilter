f85f598243030dc75b46eddcdedc65f6a77aea7c
Alexey Terekhov <alexey.terekhov@bk.ru>
2015-04-11 23:41:55 +0600

    fix default notification settings - now notifications start working before user visit «settings» window

--- app/src/main/kotlin/me/alexeyterekhov/vkfilter/GUI/SettingsActivity/Settings.kt
+++ app/src/main/kotlin/me/alexeyterekhov/vkfilter/GUI/SettingsActivity/Settings.kt
@@ -2,17 +2,15 @@ package me.alexeyterekhov.vkfilter.GUI.SettingsActivity
 
 import android.content.SharedPreferences
 
-/**
- * Created by Alexey on 09.04.2015.
- */
+
 public object Settings {
     private val KEY_NOTIFICATIONS = "pref_notifications"
     private val KEY_VIBRATION = "pref_vibration"
     private val KEY_SOUND = "pref_sound"
     private val KEY_COLOR_LIGHT = "pref_color_lights"
 
-    fun notificationsEnabled(s: SharedPreferences) = s.getBoolean(KEY_NOTIFICATIONS, false)
+    fun notificationsEnabled(s: SharedPreferences) = s.getBoolean(KEY_NOTIFICATIONS, true)
     fun allowVibration(s: SharedPreferences) = s.getBoolean(KEY_VIBRATION, true)
-    fun allowSound(s: SharedPreferences) = s.getBoolean(KEY_SOUND, true)
-    fun allowCustomLights(s: SharedPreferences) = s.getBoolean(KEY_COLOR_LIGHT, true)
+    fun allowSound(s: SharedPreferences) = s.getBoolean(KEY_SOUND, false)
+    fun allowCustomLights(s: SharedPreferences) = s.getBoolean(KEY_COLOR_LIGHT, false)
 }
