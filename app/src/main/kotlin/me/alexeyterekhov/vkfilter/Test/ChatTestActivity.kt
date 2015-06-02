f808818a526c087590f5b32fac23538e988b4e4e
Alexey <alexey.terekhov@bk.ru>
2015-06-02 12:10:35 +0600

    Add attachments width test

--- app/src/main/kotlin/me/alexeyterekhov/vkfilter/Test/ChatTestActivity.kt
+++ app/src/main/kotlin/me/alexeyterekhov/vkfilter/Test/ChatTestActivity.kt
@@ -19,7 +19,8 @@ import java.util.LinkedList
 
 public class ChatTestActivity: ChatActivity() {
     val tests = arrayListOf(
-            Pair("Вложения", { testAttachments() })
+            Pair("Вложения", { testAttachments() }),
+            Pair("Сочетания вложений", { testCombinations() })
     )
     var currentTest = 0
 
@@ -71,8 +72,8 @@ public class ChatTestActivity: ChatActivity() {
 
     private fun testAttachments() {
         clearList()
-
-        fun generateMsg(out: Boolean, state: Int = Message.STATE_SENT): Message {
+        val messages = LinkedList<Message>()
+        fun generateMessage(out: Boolean, state: Int = Message.STATE_SENT): Message {
             val m = Message("me")
             m.isOut = out
             m.text = "Текст сообщения"
@@ -81,18 +82,10 @@ public class ChatTestActivity: ChatActivity() {
             m.isRead = true
             return m
         }
-        fun generateMsgs() = arrayListOf(
-                generateMsg(true, Message.STATE_PROCESSING),
-                generateMsg(true, Message.STATE_SENT),
-                generateMsg(false, Message.STATE_SENT)
-        )
-
-        val messages = LinkedList<Message>()
-
         // Attached message
         run {
-            val msgs = generateMsgs()
-            val attachedMsg = generateMsg(true)
+            val msgs = generateAllTypesOfMessages()
+            val attachedMsg = generateMessage(true)
             msgs forEach {
                 it.attachments.messages add attachedMsg
             }
@@ -100,7 +93,7 @@ public class ChatTestActivity: ChatActivity() {
         }
         // Attached image
         run {
-            val msgs = generateMsgs()
+            val msgs = generateAllTypesOfMessages()
             val image = ImageAttachment("http://cs7060.vk.me/c621626/v621626873/2596c/SWuM7WvcR24.jpg", "", 400, 300)
             msgs forEach {
                 it.attachments.images add image
@@ -109,7 +102,7 @@ public class ChatTestActivity: ChatActivity() {
         }
         // Attached audio
         run {
-            val msgs = generateMsgs()
+            val msgs = generateAllTypesOfMessages()
             val audio = AudioAttachment("Имя исполнителя песни", "Название композиции", 192, "abc")
             msgs forEach {
                 it.attachments.audios add audio
@@ -118,7 +111,7 @@ public class ChatTestActivity: ChatActivity() {
         }
         // Attached doc
         run {
-            val msgs = generateMsgs()
+            val msgs = generateAllTypesOfMessages()
             val doc = DocAttachment("Название документа", 18 * 1024, "abc")
             msgs forEach {
                 it.attachments.documents add doc
@@ -127,16 +120,99 @@ public class ChatTestActivity: ChatActivity() {
         }
         // Attached video
         run {
-            val msgs = generateMsgs()
+            val msgs = generateAllTypesOfMessages()
             val video = VideoAttachment(1, "Название видео", 312, "http://cs7060.vk.me/c621626/v621626873/2596c/SWuM7WvcR24.jpg", "")
             msgs forEach {
                 it.attachments.videos add video
             }
             messages addAll msgs
         }
-
         getCache().putMessages(messages, true)
     }
+
+    private fun testCombinations() {
+        clearList()
+        val messages = LinkedList<Message>()
+        // Vertical distance between attachments
+        run {
+            val msgs = generateAllTypesOfMessages()
+            val audio1 = AudioAttachment("Имя исполнителя песни", "Название композиции", 192, "abc")
+            val audio2 = AudioAttachment("Имя исполнителя песни2", "Название композиции2", 192, "abc")
+            msgs forEach {
+                it.text = "Вертикальное расстояние между вложениями"
+                it.attachments.audios add audio1
+                it.attachments.audios add audio2
+            }
+            messages addAll msgs
+        }
+        // Text width vs attachment width
+        run {
+            val msgs = generateAllTypesOfMessages()
+            val audio = AudioAttachment("Имя исполнителя песни", "Название композиции", 192, "abc")
+            msgs forEach {
+                it.text = "Small"
+                it.attachments.audios add audio
+            }
+            messages addAll msgs
+        }
+        run {
+            val msgs = generateAllTypesOfMessages()
+            val audio = AudioAttachment("A", "B", 192, "abc")
+            msgs forEach {
+                it.text = "Veeeeeeeeryyyyyy bbbiiiiiiiggggg teeeeeeeexxxxttttt"
+                it.attachments.audios add audio
+            }
+            messages addAll msgs
+        }
+        run {
+            val msgs = generateAllTypesOfMessages()
+            val image = ImageAttachment("http://cs7060.vk.me/c621626/v621626873/2596c/SWuM7WvcR24.jpg", "", 400, 300)
+            msgs forEach {
+                it.text = "Small"
+                it.attachments.images add image
+            }
+            messages addAll msgs
+        }
+        run {
+            val msgs = generateAllTypesOfMessages()
+            val image = ImageAttachment("http://cs7060.vk.me/c621626/v621626873/2596c/SWuM7WvcR24.jpg", "", 400, 300)
+            msgs forEach {
+                it.text = "Veeeeeeeeryyyyyy bbbiiiiiiiggggg teeeeeeeexxxxttttt"
+                it.attachments.images add image
+            }
+            messages addAll msgs
+        }
+        // Attachment width vs attachment width
+        run {
+            val msgs = generateAllTypesOfMessages()
+            val audio1 = AudioAttachment("Имя исполнителя песни", "Название композиции", 192, "abc")
+            val audio2 = AudioAttachment("A", "B", 192, "abc")
+            msgs forEach {
+                it.text = "Audio width vs"
+                it.attachments.audios add audio1
+                it.attachments.audios add audio2
+            }
+            messages addAll msgs
+        }
+        getCache().putMessages(messages, true)
+    }
+
+    // Util methods
+
+    private fun generateMessage(out: Boolean, state: Int = Message.STATE_SENT): Message {
+        val m = Message("me")
+        m.isOut = out
+        m.text = "Текст сообщения"
+        m.sentTimeMillis = System.currentTimeMillis()
+        m.sentState = state
+        m.isRead = true
+        return m
+    }
+    private fun generateAllTypesOfMessages() = arrayListOf(
+            generateMessage(false, Message.STATE_SENT),
+            generateMessage(true, Message.STATE_SENT),
+            generateMessage(true, Message.STATE_PROCESSING)
+    )
 
     private fun clearList() {
         val adapter = getAdapter()
