0694cd9e73fbc37a1d7be67c5b9c590e0870f235
Alexey <alexey.terekhov@bk.ru>
2015-06-08 13:36:54 +0600

    animate chat activity panels, smiles etc

--- app/src/main/kotlin/me/alexeyterekhov/vkfilter/GUI/ChatActivityNew/EditPanelModule.kt
+++ app/src/main/kotlin/me/alexeyterekhov/vkfilter/GUI/ChatActivityNew/EditPanelModule.kt
@@ -1,7 +1,10 @@
 package me.alexeyterekhov.vkfilter.GUI.ChatActivityNew
 
+import android.support.v4.view.animation.PathInterpolatorCompat
 import android.text.Editable
 import android.text.TextWatcher
+import android.view.animation.Animation
+import android.view.animation.ScaleAnimation
 import android.widget.EditText
 import android.widget.ImageView
 import me.alexeyterekhov.vkfilter.DataCache.MessageCaches
@@ -16,7 +19,6 @@ class EditPanelModule(val activity: ChatActivity) {
     fun onCreate() {
         fillMessageInput()
         initSendButton()
-        initAttachmentButtons()
     }
 
     fun onDestroy() {
@@ -24,13 +26,19 @@ class EditPanelModule(val activity: ChatActivity) {
         text.removeTextChangedListener(textListener)
     }
 
-    fun bindSendButton(iconRes: Int, action: () -> Unit) {
+    fun bindSendButton(iconRes: Int, action: () -> Unit, animate: Boolean = false) {
         bindAction = action
-        activity.findViewById(R.id.sendButton) as ImageView setImageResource iconRes
+        if (animate)
+            animateSendButtonIconChange(iconRes)
+        else
+            activity.findViewById(R.id.sendButton) as ImageView setImageResource iconRes
     }
-    fun unbindSendButton() {
+    fun unbindSendButton(animate: Boolean = false) {
         bindAction = null
-        activity.findViewById(R.id.sendButton) as ImageView setImageResource R.drawable.button_send
+        if (animate)
+            animateSendButtonIconChange(R.drawable.button_send)
+        else
+            activity.findViewById(R.id.sendButton) as ImageView setImageResource R.drawable.button_send
     }
     fun getEditText() = activity.findViewById(R.id.messageText) as KeyboardlessEmojiEditText
 
@@ -58,12 +66,6 @@ class EditPanelModule(val activity: ChatActivity) {
         }
     }
 
-    private fun initAttachmentButtons() {
-        activity.findViewById(R.id.smileButton) setOnClickListener {
-            activity.emojiconModule.openEmojiconPanel()
-        }
-    }
-
     private fun saveEditMessage() {
         val editMessage = getMessageCache().getEditMessage()
         val textInput = activity.findViewById(R.id.messageText) as EditText
@@ -76,8 +78,44 @@ class EditPanelModule(val activity: ChatActivity) {
     )
 
     private fun createTextListener() = object : TextWatcher {
-        override fun afterTextChanged(s: Editable?) = saveEditMessage()
+        override fun afterTextChanged(s: Editable?) {
+            saveEditMessage()
+            if (activity.swipePanelModule.isPanelShown())
+                (activity.findViewById(R.id.sendButton) as ImageView).performClick()
+        }
         override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {}
         override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+    }
+
+    private fun animateSendButtonIconChange(changeImgToRes: Int) {
+        val button = activity.findViewById(R.id.sendButton) as ImageView
+
+        val downScale = ScaleAnimation(
+                1.0f, 0f,
+                1.0f, 0f,
+                Animation.RELATIVE_TO_SELF, 0.5f,
+                Animation.RELATIVE_TO_SELF, 0.5f
+        )
+        downScale setInterpolator PathInterpolatorCompat.create(0.4f, 0f, 0.2f, 1f)
+        downScale setDuration 100
+
+        val upScale = ScaleAnimation(
+                0f, 1.0f,
+                0f, 1.0f,
+                Animation.RELATIVE_TO_SELF, 0.5f,
+                Animation.RELATIVE_TO_SELF, 0.5f
+        )
+        upScale setInterpolator PathInterpolatorCompat.create(0.4f, 0f, 0.2f, 1f)
+        upScale setDuration 100
+
+        downScale setAnimationListener object : Animation.AnimationListener {
+            override fun onAnimationEnd(animation: Animation?) {
+                button setImageResource changeImgToRes
+                button startAnimation upScale
+            }
+            override fun onAnimationStart(animation: Animation?) {}
+            override fun onAnimationRepeat(animation: Animation?) {}
+        }
+        button startAnimation downScale
     }
 }
--- app/src/main/kotlin/me/alexeyterekhov/vkfilter/GUI/ChatActivityNew/EmojiconModule.kt
+++ app/src/main/kotlin/me/alexeyterekhov/vkfilter/GUI/ChatActivityNew/EmojiconModule.kt
@@ -1,7 +1,11 @@
 package me.alexeyterekhov.vkfilter.GUI.ChatActivityNew
 
+import android.animation.Animator
+import android.animation.ValueAnimator
 import android.os.Bundle
 import android.os.Handler
+import android.support.v4.view.animation.PathInterpolatorCompat
+import android.util.TypedValue
 import android.view.View
 import com.rockerhieu.emojicon.EmojiconGridFragment
 import com.rockerhieu.emojicon.EmojiconsFragment
@@ -12,21 +16,23 @@ class EmojiconModule(val activity: ChatActivity) :
         EmojiconsFragment.OnEmojiconBackspaceClickedListener,
         EmojiconGridFragment.OnEmojiconClickedListener
 {
-    val DELAY_BEFORE_OPEN = 500L
+    val PORTRAIT_HEIGHT = 250f
+    val LANDSCAPE_HEIGHT = 130f
+    val DELAY_BEFORE_OPEN = 300L
     val KEY_MODULE_OPENED = "emoji_open"
     private val closeEmojiPanelAction: (() -> Unit) = {
         hidePanel()
         Handler().postDelayed({
             activity.editPanelModule.getEditText().allowKeyboardAndShow(true)
-            activity.editPanelModule.unbindSendButton()
+            activity.editPanelModule.unbindSendButton(animate = true)
         }, DELAY_BEFORE_OPEN)
     }
 
     fun onCreate(saved: Bundle?) {
         if (saved != null && saved containsKey KEY_MODULE_OPENED) {
             activity.editPanelModule.getEditText().denyKeyboard()
-            bindForClosingSmiles()
-            showPanel()
+            bindForClosingSmiles(animate = false)
+            showPanel(animate = false)
         }
     }
     fun onSaveState(bundle: Bundle?) {
@@ -37,22 +43,93 @@ class EmojiconModule(val activity: ChatActivity) :
     }
 
     fun openEmojiconPanel() {
-        activity.editPanelModule.getEditText().denyKeyboard()
-        Handler().postDelayed({
-            showPanel()
-            bindForClosingSmiles()
-        }, DELAY_BEFORE_OPEN)
+        if (!isEmojiconPanelShown()) {
+            activity.editPanelModule.getEditText().denyKeyboard()
+            Handler().postDelayed({
+                showPanel(animate = true)
+                bindForClosingSmiles(animate = true)
+            }, DELAY_BEFORE_OPEN)
+        } else {
+            bindForClosingSmiles(animate = false)
+        }
     }
-    fun bindForClosingSmiles() {
+    fun bindForClosingSmiles(animate: Boolean = false) {
         activity.editPanelModule.bindSendButton(
                 iconRes = R.drawable.button_close,
-                action = closeEmojiPanelAction
+                action = closeEmojiPanelAction,
+                animate = animate
         )
     }
 
-    private fun hidePanel() = activity.findViewById(R.id.emoji_container).setVisibility(View.GONE)
-    private fun showPanel() = activity.findViewById(R.id.emoji_container).setVisibility(View.VISIBLE)
+    private fun hidePanel() {
+        val panel = activity.findViewById(R.id.emoji_container)
+        val fragment = activity.findViewById(R.id.emoji_fragment)
+
+        val animator = ValueAnimator.ofFloat(getHeight(), 0f)
+        animator addUpdateListener { animator ->
+            val curHeight = animator.getAnimatedValue() as Float
+            val params = panel.getLayoutParams()
+            params.height = curHeight.toInt()
+            panel.setLayoutParams(params)
+        }
+        animator setInterpolator PathInterpolatorCompat.create(0.4f, 0f, 0.2f, 1f)
+        animator setDuration 150
+        animator addListener object : Animator.AnimatorListener {
+            override fun onAnimationRepeat(animation: Animator?) {}
+            override fun onAnimationCancel(animation: Animator?) {}
+            override fun onAnimationStart(animation: Animator?) {}
+            override fun onAnimationEnd(animation: Animator) {
+                panel setVisibility View.GONE
+            }
+        }
+        fragment.setVisibility(View.GONE)
+        animator.start()
+    }
+    private fun showPanel(animate: Boolean = false) {
+        val panel = activity.findViewById(R.id.emoji_container)
+        val fragment = activity.findViewById(R.id.emoji_fragment)
+
+        if (animate) {
+            fragment.setVisibility(View.GONE)
+            val animator = ValueAnimator.ofFloat(0f, getHeight())
+            animator addUpdateListener { animator ->
+                val curHeight = animator.getAnimatedValue() as Float
+                val params = panel.getLayoutParams()
+                params.height = curHeight.toInt()
+                panel.setLayoutParams(params)
+            }
+            animator setInterpolator PathInterpolatorCompat.create(0.4f, 0f, 0.2f, 1f)
+            animator setDuration 150
+            animator addListener object : Animator.AnimatorListener {
+                override fun onAnimationRepeat(animation: Animator?) {}
+                override fun onAnimationCancel(animation: Animator?) {}
+                override fun onAnimationStart(animation: Animator?) {}
+                override fun onAnimationEnd(animation: Animator) {
+                    fragment setVisibility View.VISIBLE
+                }
+            }
+            panel.setVisibility(View.VISIBLE)
+            animator.start()
+        } else {
+            val params = panel.getLayoutParams()
+            params.height = getHeight().toInt()
+            panel.setLayoutParams(params)
+            panel.setVisibility(View.VISIBLE)
+            fragment.setVisibility(View.VISIBLE)
+        }
+    }
     fun isEmojiconPanelShown() = activity.findViewById(R.id.emoji_container).getVisibility() == View.VISIBLE
+
+    fun getHeight(): Float {
+        val defaultDisplay = activity.getWindowManager().getDefaultDisplay()
+        val isLandscape = defaultDisplay.getWidth() > defaultDisplay.getHeight()
+        val displayMetrics = activity.getResources().getDisplayMetrics()
+        return TypedValue.applyDimension(
+                TypedValue.COMPLEX_UNIT_DIP,
+                if (isLandscape) LANDSCAPE_HEIGHT else PORTRAIT_HEIGHT,
+                displayMetrics
+        )
+    }
 
     override fun onEmojiconBackspaceClicked(p0: View?) = EmojiconsFragment.backspace(activity.editPanelModule.getEditText())
     override fun onEmojiconClicked(emoji: Emojicon?) = EmojiconsFragment.input(activity.editPanelModule.getEditText(), emoji)
