b9cefcc1c27482f94090e6cd8185c154670a0fcd
Alexey <alexey.terekhov@bk.ru>
2015-07-26 10:50:47 +0800

    better and easier "sending" tinting: messages in state "sending" now tints their attachments, smiles and other content

--- app/src/main/kotlin/me/alexeyterekhov/vkfilter/GUI/ChatActivity/MessageList/HolderMessageOut.kt
+++ app/src/main/kotlin/me/alexeyterekhov/vkfilter/GUI/ChatActivity/MessageList/HolderMessageOut.kt
@@ -3,27 +3,21 @@ package me.alexeyterekhov.vkfilter.GUI.ChatActivity.MessageList
 import android.view.View
 import me.alexeyterekhov.vkfilter.DataClasses.Message
 import me.alexeyterekhov.vkfilter.R
-import me.alexeyterekhov.vkfilter.Util.AppContext
 
 class HolderMessageOut(view: View): HolderMessageBase(view) {
+    val sendingTint = view.findViewById(R.id.messageSendingTint)
+
     // Colors
     fun setColorsForState(messageState: Int) {
-        when (messageState) {
-            Message.STATE_SENDING -> {
-                messageBody setBackgroundResource R.drawable.message_sending_background
-                messageTriangle setBackgroundResource R.drawable.message_sending_triangle
-                messageText setTextColor AppContext.instance.getResources().getColor(R.color.ui_msg_font_sending)
-            }
-            Message.STATE_SENT -> {
-                messageBody setBackgroundResource R.drawable.message_out_background
-                messageTriangle setBackgroundResource R.drawable.message_out_triangle
-                messageText setTextColor AppContext.instance.getResources().getColor(R.color.ui_msg_font)
-            }
-        }
+        sendingTint setVisibility if (messageState == Message.STATE_SENDING)
+            View.VISIBLE
+        else
+            View.GONE
+        messageBody setBackgroundResource R.drawable.message_out_background
+        messageTriangle setBackgroundResource R.drawable.message_out_triangle
     }
     fun setColorsSelected() {
         messageBody setBackgroundResource R.drawable.message_sel_background
         messageTriangle setBackgroundResource R.drawable.message_out_sel_triangle
-        messageText setTextColor AppContext.instance.getResources().getColor(R.color.ui_msg_font)
     }
 }
